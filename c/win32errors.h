d0788de868cd83aeca9310811921dcd422d9171b
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-02-12 15:06:59 +0100

    Fixed .h header file generation (missing function prototypes)

--- c/win32errors.h
+++ c/win32errors.h
@@ -5,5 +5,7 @@
 
 const char* lookup_errorA(unsigned long errcode);
 const wchar_t* lookup_errorW(unsigned long errcode);
+const char* lookup_error_with_nameA(unsigned long errcode);
+const wchar_t* lookup_error_with_nameW(unsigned long errcode);
 
 #endif

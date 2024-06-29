88b77b2163c8e0d85bdb97bbafadd2dca94a2326
Rémi GASCOU (Podalirius) <podalirius@protonmail.com>
2024-06-29 12:08:42 -0700

    Update ExampleProject.cpp

--- c/ExampleProject/ExampleProject/ExampleProject.cpp
+++ c/ExampleProject/ExampleProject/ExampleProject.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <windows.h>
 
-extern "C" {
 #include "../../win32errors.h"
-}
+
 
 int main()
 {
-    wprintf(lookup_errorW(0));
+    unsigned long errcode = 0x00003cfd;
+
+    // https://stackoverflow.com/a/17386923
+    wchar_t buf[4096];
+    FormatMessageW(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, // dwFlags
+        NULL,                                                       // lpSource
+        errcode,                                                    // dwMessageId
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),                  // dwLanguageId
+        buf,                                                        // lpBuffer
+        (sizeof(buf) / sizeof(wchar_t)),                            // nSize
+        NULL                                                        // *Arguments
+    );
+    wprintf(L"[+] With builtin FormatMessageW():\r\n");
+    wprintf(L"%s\r\n\n", buf);
+
+    // Win32ErrorCodes
+    wprintf(L"[+] With Win32ErrorCodes library:\r\n");
+    wprintf(L"%s\r\n", lookup_errorW(errcode));
 }
